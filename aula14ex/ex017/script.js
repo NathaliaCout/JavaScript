9a9bbe1b7a529351b71e89e198f3795ad83b67ed
NathaliaCout <coutinhona@hotmail.com>
2025-01-24 18:34:18 -0300

    up 17

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,19 +1,22 @@
 function gerartabuada(){
     var num = window.document.getElementById('txtnum')
     var res = window.document.querySelector('div#res')
-
-    var n = Number(num.value)
-
+    var tab = window.document.getElementById('seltab')
+    
     if (num.value.length == 0){
         window.alert('Por favor, digite um número!')
     } else {
+        var n = Number(num.value)
+        tab.innerHTML = ''
         for (var c = 1; c <=10 ; c++){
             var r = n * c 
-            res.innerHTML += (`${n} x ${c} = ${r} <br>`)
+            //res.innerHTML += (`${n} x ${c} = ${r} <br>`)
+
+            //Exibir usando selct
+            var item = window.document.createElement('option')
+            item.text = `${n} x ${c} = ${r} `
+            item.value = `tab${c}`
+            tab.appendChild(item)
         }
-
-    }  
-
-    
-
+    }
 }

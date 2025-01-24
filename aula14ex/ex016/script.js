37779b2a1030bfb8ced395984cfcc996d6d0ee2d
NathaliaCout <coutinhona@hotmail.com>
2025-01-24 13:11:57 -0300

    up ex 16

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -5,24 +5,33 @@ function contagem(){
     var res = window.document.getElementById('res')
 
     if (comeco.value.length == 0 || ate.value.length == 0 || pas.value.length == 0){
-        window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar!'
+        //window.alert('[ERRO] Faltam dados!')
+        
     }else {
-        res.innerHTML = `contando: `
+        res.innerHTML = `Contando: <br>`
         var i = Number(comeco.value)
         var f = Number(ate.value)
         var p = Number(pas.value)
+
+        if(p <=0){
+            window.alert('Passo inválido! Considerando PASSO = 1')
+            p = 1
+        }
         
         if (i < f){
+            //Contagem crescente
             for(var c = i; c <= f; c += p){
                 res.innerHTML += `${c} \u{1F449} `
             } 
 
         }else{
+            //Contagem regressiva
             for(var c = i; c>=f; c -= p){
                 res.innerHTML += `${c} \u{1F449} `
             }
-            res.innerHTML+= `\u{1F3C1}`
         }
+        res.innerHTML+= `\u{1F3C1}`
 
         
     }

84209df49c55e678114a4c505511a451996f1fb7
NathaliaCout <coutinhona@hotmail.com>
2025-02-10 10:45:34 -0300

    up

---
+++ aula16ex/ex018/script.js
@@ -0,0 +1,19 @@
+function adicionar(){
+
+    let n = window.document.getElementById('txtnum')
+    let resul = window.document.getElementById('res')
+       
+    if (n.length == 0){
+        window.alert('Favor insira um numero!')
+    }else{
+        //adione o numero:
+        
+        resul.querySelector = n
+
+           // nu = Number(n.value)
+
+        
+
+    }
+
+}
